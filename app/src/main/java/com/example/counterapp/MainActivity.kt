2c31c767bbeeccede872c770a964c2872426f5a1
ilinaos <ilinaos@ya.ru>
2021-08-10 15:49:49 +0500

    first commit

---
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.counterapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById <TextView> (R.id.textView)
+    }
+
+    fun Reset (view: View) {
+        textView.text="0";
+    }
+
+    fun Count (view: View) {
+        val num = textView.text.toString()
+        var count: Int=Integer.parseInt(num)
+        count++
+        textView.text=count.toString();
+    }
+}
